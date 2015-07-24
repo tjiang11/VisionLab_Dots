57a843d627611bf0b6352590b1b0bb6186b71f9b
tjiang11 <tjiang11@jhu.edu>
2015-07-24 15:27:44 -0400

    Changed number of stars per bg to 3

--- src/controller/DotsGameController.java
+++ src/controller/DotsGameController.java
@@ -101,7 +101,7 @@ public class DotsGameController implements GameController {
     private static int numStars = 0;
     
     /** Number of stars earned before changing to next background. */
-    private static final int STARS_PER_BACKGROUND = 2;
+    private static final int STARS_PER_BACKGROUND = 3;
     
     private enum GameState {
         /** Player has responded and next round is loading. */
